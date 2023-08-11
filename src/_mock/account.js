7052a94bdf7fa459d1efcb30588b537c9a033b0f
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-08-11 22:14:43 +0300

    admin1

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Jaydon Frankie',
+  email: 'demo@minimals.cc',
+  photoURL: 'https://sxprotection.com.au/wp-content/uploads/2016/07/team-placeholder.png',
+};
+
+export default account;
